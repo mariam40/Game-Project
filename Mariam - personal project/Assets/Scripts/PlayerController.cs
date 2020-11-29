c6b1788b23a03c447d9287934ca5e2dc744f446e
mariam40 <B00107841@mytudublin.ie>
2020-11-29 17:39:09 +0000

    Game Project

---
+++ Mariam - personal project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 200.0f;
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("obstacle"))
+        {
+            Debug.Log("Player had collided with obstacle.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Cookie")) ;
+        {
+            Debug.Log("Player collected cookie");
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Bonus")) ;
+        {
+            Debug.Log("Player collected bonus cookie");
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("PowerUp")) ;
+        {
+            Debug.Log("Player gained a power up");
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Mariam - personal project/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstacles;
+    public GameObject[] collect;
+
+    private float ObstaclesSpawnZ = 12.0f;
+    private float SpawnRangeX = 16.0f;
+    private float CollectRange = 5.0f;
+    private float SpawnY = 0.75f;
+
+    private float collectSpawnTime = 5.0f;
+    private float obstacleSpawnTime = 1.0f;
+    private float startDelay = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacles", startDelay, obstacleSpawnTime);
+        InvokeRepeating("SpawnCollect", startDelay, collectSpawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacles()
+    {
+        float randomX = Random.Range(-SpawnRangeX, SpawnRangeX);
+        int randomIndex = Random.Range(0, obstacles.Length);
+
+        Vector3 spawnPos = new Vector3(randomX, SpawnY, ObstaclesSpawnZ);
+
+        Instantiate(obstacles[randomIndex], spawnPos, obstacles[randomIndex].gameObject.transform.rotation);
+    }
+
+    void SpawnCollect()
+    {
+        float randomX = Random.Range(-SpawnRangeX, SpawnRangeX);
+        int randomIndex = Random.Range(0, collect.Length);
+
+        Vector3 spawnPos = new Vector3(randomX, SpawnY, CollectRange);
+
+        Instantiate(collect[randomIndex], spawnPos, collect[randomIndex].gameObject.transform.rotation);
+    }
+}
